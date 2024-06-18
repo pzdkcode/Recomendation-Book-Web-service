0834a2991426c290b605cb5e747f94ffd6f5ebd3
pzdkcode <maloyjunk@ya.ru>
2024-06-18 18:58:32 +0300

    Update script.js

--- includes/script.js
+++ includes/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const prompt = `Жанры: ${genres}\nАвторы: ${authors}\nКниги которые нравятся: ${likedBooks}\nОписание: ${description}\nПожалуйста, порекомендуй 3 книги.`;
 
     // Отправка данных на серверную функцию Vercel
-   fetch('https:/recomendation-book-web-service-bs1b.vercel.app/api/recomendation', {
+   fetch('https:/recomendation-book-web-service-bs1b.vercel.app/api/recomendation.js', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
